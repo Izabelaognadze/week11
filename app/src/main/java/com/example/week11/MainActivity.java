2fb07c9129c5ed448d99bb581e0aeeb11f04699e
izabela <isabella.ognadze@gmail.com>
2023-05-20 17:43:35 +0400

    initial commit

--- app/src/main/java/com/example/week11/MainActivity.java
+++ app/src/main/java/com/example/week11/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.week11;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.week11.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment((new HomeFragment()));
+                    break;
+                case R.id.message:
+                    replaceFragment((new messageFragment()));
+                    break;
+                case R.id.notification:
+                    replaceFragment((new notificationFragment()));
+                    break;
+                case R.id.settings:
+                    replaceFragment((new settingsFragment()));
+                    break;
+                case R.id.reaction:
+                    replaceFragment((new reactionFragment()));
+                    break;
+            }
+            return true;
+        });
+
+
+    }
+
+    private void replaceFragment(Fragment Fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
     }
 }
--- app/src/main/java/com/example/week11/reactionFragment.java
+++ app/src/main/java/com/example/week11/reactionFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link fragment_reaction#newInstance} factory method to
+ * Use the {@link reactionFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_reaction extends Fragment {
+public class reactionFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class fragment_reaction extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public fragment_reaction() {
+    public reactionFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class fragment_reaction extends Fragment {
      * @return A new instance of fragment fragment_reaction.
      */
     // TODO: Rename and change types and number of parameters
-    public static fragment_reaction newInstance(String param1, String param2) {
-        fragment_reaction fragment = new fragment_reaction();
+    public static reactionFragment newInstance(String param1, String param2) {
+        reactionFragment fragment = new reactionFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
